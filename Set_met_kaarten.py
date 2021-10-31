a5057c93983563adf3d016d6ba88709413e6b565
Abdullah <ab-benkathir@outlook.com>
2021-10-31 21:35:49 +0100

    F1.6.02.O3 - Set met kaarten

---
+++ Set_met_kaarten.py
@@ -0,0 +1,35 @@
+import random
+
+card = []
+a = ["boer " , "vrouw ", "heer " ,"aas " ]
+colors = ["harten ", "klaveren ", "schoppen ", "ruiten "]
+joker = ["joker" , "joker"]
+deck = []
+
+for i in range (2,14):
+    card.append(str( i))
+
+for j in range (4):
+    card.append(a[ j])
+
+
+for k in range(4):
+    for i in range(13):
+        card1 = (colors[k] + card[ i] )
+        deck.append(card1)
+
+for n in range(2):
+    deck.append(joker[n])
+
+
+random.shuffle(deck)
+
+for i in range (7):
+    z = random.choice(deck)
+    print(z)
+    deck.remove(z)
+
+print(len(deck))
+
+
+
